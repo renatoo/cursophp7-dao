8f33f2f0878e93b6ca18932c63eb3aa349496827
Renatoo <webrenat@gmail.com>
2018-01-05 17:12:03 -0200

    inserir e alterar

--- index.php
+++ index.php
@@ -10,25 +10,34 @@ require_once("config.php");
 
 //CARREGA UM USUÁRIO
 // $root = new Usuario();
-//
 // $root->loadById(4);
 //
 // echo $root;
 
 //CARREGA UMA LISTA
 // $lista = Usuario::getList();
-//
 // echo json_encode($lista);
 
 //CARREGA UMA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN
 // $search = Usuario::search("ma");
-//
 // echo json_encode($search);
 
 //CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
-$usuario = new Usuario();
+// $usuario = new Usuario();
+// $usuario->login("marzo", "bento45");
+//
+// echo $usuario;
 
-$usuario->login("marzo", "bento45");
+//CRIAR UM NOVO USUÁRIO
+// $aluno = new Usuario("student", "poi987");
+// $aluno->insert();
+//
+// echo $aluno;
+
+//ATUALIZAR USÁRIO
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->update("professor", "prof123");
 
 echo $usuario;
 
